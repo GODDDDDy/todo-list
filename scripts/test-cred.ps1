f87ef6d7f68aa7ad87d21cd3a3cfb9ee284e097d
GODDDDDy <GODDDDDy@users.noreply.github.com>
2026-07-14 15:52:11 +0800

    chore: remove temp test script

--- scripts/test-cred.ps1
+++
@@ -1,6 +0,0 @@
-$tmp = [System.IO.Path]::GetTempFileName()
-[System.IO.File]::WriteAllText($tmp, "protocol=https`nhost=github.com`n`n", [System.Text.Encoding]::ASCII)
-$result = & cmd /c "git credential fill < `"$tmp`" 2>&1"
-Remove-Item $tmp -ErrorAction SilentlyContinue
-Write-Output "RESULT:"
-Write-Output $result
